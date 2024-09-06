90ed8b3ec96b567cab021ee912529bfb0b73e27d
Alejandro Castro <alejo199821@gmail.com>
2024-09-06 17:22:16 -0400

    complete desktop styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,11 +17,11 @@ const Footer = () => {
         <div className="footer__wrapper">
             <div className="footer__content">
                 <h5 className='footer__header'>Thank you for Visiting</h5>
-                <p>I appreciate the time you took out of your day to check out my portfolio. Have a nice day.</p>
+                <p className='footer__text'>I appreciate the time you took out of your day to check out my portfolio. Have a nice day.</p>
             </div>
             <ul className="contact__icons footer__icons">
                 <li>
-                    <a href="https://www.linkedin.com/in/alejandro-castro-professional/" className="contact_link">
+                    <a href="https://www.linkedin.com/in/alejandro-castro-professional/" className="contact_link footer__contact__link">
                         <div className="icon-container footer__icon-container">
                             <img src={linkedIn}  className="contact__icon footer__contact_icon" alt="linkedin logo"/>
                         </div>
@@ -29,7 +29,7 @@ const Footer = () => {
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/alejo2198" className="contact_link">
+                    <a href="https://github.com/alejo2198" className="contact_link footer__contact__link">
                         <div className="icon-container footer__icon-container">
                             <img src={github} className="contact__icon footer__contact_icon"  alt="github logo"/>
                         </div>
@@ -37,7 +37,7 @@ const Footer = () => {
                     </a>
                 </li>
                 <li>
-                    <a href="https://docs.google.com/document/d/1k4IQN1LOXPvVGYoyf9VfNVUcsgeGms5naMfbv3h5wEA/edit?usp=sharing" className="contact_link">
+                    <a href="https://docs.google.com/document/d/1k4IQN1LOXPvVGYoyf9VfNVUcsgeGms5naMfbv3h5wEA/edit?usp=sharing" className="contact_link footer__contact__link">
                         <div className="icon-container footer__icon-container">
                             <img src={resume} className="contact__icon resume footer__contact_icon"  alt="resume icon"/>
                         </div>
@@ -45,7 +45,7 @@ const Footer = () => {
                     </a>
                 </li>
                 <li>
-                    <a href="tel:6475455417" className="contact_link">
+                    <a href="tel:6475455417" className="contact_link footer__contact__link">
                         <div className="icon-container footer__icon-container">
                             <img src={phone}  className="contact__icon footer__contact_icon" alt="phone icon"/>
                         </div>
@@ -53,7 +53,7 @@ const Footer = () => {
                     </a>
                 </li>
                 <li>
-                    <a href="mailto:alejo199821@gmail.com" className="contact_link">
+                    <a href="mailto:alejo199821@gmail.com" className="contact_link footer__contact__link">
                         <div className="icon-container footer__icon-container">
                             <img src={email}  className="contact__icon footer__contact_icon" alt="email icon"/>
                         </div>
